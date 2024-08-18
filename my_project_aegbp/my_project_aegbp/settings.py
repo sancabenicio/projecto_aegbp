4ff87c29d2563701ed9f42dfdc7f0f31b008fd8c
sancabenicio <beniciosanca224@gmail.com>
2024-08-18 11:44:57 +0100

    atualização no requriments.txt

--- my_project_aegbp/my_project_aegbp/settings.py
+++ my_project_aegbp/my_project_aegbp/settings.py
@@ -245,11 +245,6 @@ LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
 
 MODELTRANSLATION_DEFAULT_LANGUAGE = 'pt'
 
-# Arquivos estáticos e mídia
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Tipo de campo padrão para chaves primárias
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
